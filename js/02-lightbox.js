56c2a395d3d3b2dcae8962c12815519366600385
Yevhenii2022 <3starosta3@gmail.com>
2023-03-04 08:06:18 +0200

    add lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+function createGalleryLink(links) {
+	return links
+		.map(({ preview, original, description }) => {
+			return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+    `;
+		})
+		.join('');
+}
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = createGalleryLink(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
 
 console.log(galleryItems);
